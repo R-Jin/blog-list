a3e55963ba0cef0e45f01f71ebbe96de23e6d188
Ryan Jin <ryanjin007@gmail.com>
2024-05-31 22:31:07 +0200

    Refactor code
    Make a dedicated app module

---
+++ app.js
@@ -0,0 +1,26 @@
+const config = require("./utils/config");
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Failed to connect to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const cors = require("cors");
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-
+const app = require("./app");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-const blogsRouter = require("./controllers/blogs");
-
-mongoose.connect(config.MONGODB_URI);
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/blogs", blogsRouter);
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
