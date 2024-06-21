276ac98215b8b75be77accebf3d33ac780004cb6
Ryan Jin <ryanjin007@gmail.com>
2024-06-21 12:08:23 +0200

    Complete exercise 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,7 +1,7 @@
 const { test, after, beforeEach } = require("node:test")
 const assert = require("node:assert")
 const Blog = require("../models/blog")
-const { initialBlogs } = require("./testInput")
+const { initialBlogs, listWithOneBlog } = require("./testInput")
 const mongoose = require("mongoose")
 const supertest = require("supertest")
 const app = require("../app")
@@ -26,6 +26,24 @@ test("Unique identifier of blog posts is 'id'", async () => {
     
     const blogs = response.body
     assert(blogs.every(blog => "id" in blog))
+})
+
+test("Correctly creates new blog post on POST request", async () => {
+    const blogToCreate = listWithOneBlog[0]
+    await api
+        .post("/api/blogs", blogToCreate)
+        .expect(201)
+        .expect("Content-Type", /application\/json/)
+
+    const response = await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/)
+    
+    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+    const contents = response.body.map(blog => blog.url)
+    assert(contents.includes(listWithOneBlog.url))
 })
 
 // Prepare the database with initial data
