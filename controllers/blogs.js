61ccdc3befc5fa3e0e0bcf4daf29a11f97352803
Ryan Jin <ryanjin007@gmail.com>
2024-06-22 21:40:41 +0200

    Add put request

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -22,4 +22,22 @@ blogsRouter.delete("/:id", async (request, response) => {
   response.status(204).end()
 })
 
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body
+  if (body.title && body.author && body.url && body.likes) {
+    const updatedBlog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    }
+
+    const result = await Blog.findByIdAndUpdate(request.params.id, updatedBlog, {new: true})
+    response.json(result)
+  } else {
+    response.status(400).end()
+  }
+
+})
+
 module.exports = blogsRouter;
